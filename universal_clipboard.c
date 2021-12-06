d9a02049a57dd93aae1e400f3f6cf77cdb38df26
Jacob Malloy <sjakem@att.net>
2021-12-06 17:31:45 -0600

    modify for yed version 1300 to use built in get selection

--- universal_clipboard.c
+++ universal_clipboard.c
@@ -13,7 +13,7 @@ static yed_plugin *global_self;
 
 static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 //code modified from stack overflow post to get the b4 bit encoded verison of a char array
-char *b64_encode(const unsigned char *in, size_t len)
+char *b64_encode(const char *in, size_t len)
 {
     char   *out;
     size_t  elen;
@@ -52,63 +52,6 @@ char *b64_encode(const unsigned char *in, size_t len)
     }
 
     return out;
-}
-
-//function given by Brandon Kammerdiener to return the char array for the selected text.
-char *get_sel_text(yed_buffer *buffer) {
-    char      nl;
-    array_t   chars;
-    int       r1;
-    int       c1;
-    int       r2;
-    int       c2;
-    int       r;
-    yed_line *line;
-    int       cstart;
-    int       cend;
-    int       i;
-    int       n;
-    char     *data;
-
-    nl    = '\n';
-    chars = array_make(char);
-
-    yed_range_sorted_points(&buffer->selection, &r1, &c1, &r2, &c2);
-
-    if (buffer->selection.kind == RANGE_LINE) {
-        for (r = r1; r <= r2; r += 1) {
-            line = yed_buff_get_line(buffer, r);
-            if (line == NULL) { break; } /* should not happen */
-
-            data = (char*)array_data(line->chars);
-            array_push_n(chars, data, array_len(line->chars));
-            array_push(chars, nl);
-        }
-    } else {
-        for (r = r1; r <= r2; r += 1) {
-            line = yed_buff_get_line(buffer, r);
-            if (line == NULL) { break; } /* should not happen */
-
-            if (line->visual_width > 0) {
-                cstart = r == r1 ? c1 : 1;
-                cend   = r == r2 ? c2 : line->visual_width + 1;
-
-                i    = yed_line_col_to_idx(line, cstart);
-                n    = yed_line_col_to_idx(line, cend) - i;
-                data = array_item(line->chars, i);
-
-                array_push_n(chars, data, n);
-            }
-
-            if (r < r2) {
-                array_push(chars, nl);
-            }
-        }
-    }
-
-    array_zero_term(chars);
-
-    return (char*)array_data(chars);
 }
 
 int yed_plugin_boot(yed_plugin *self){
@@ -150,7 +93,7 @@ void copy_to_universal_clipboard(int argc, char **argv){
     char * out_buffer;
     yed_buffer *buffer = ys->active_frame!=NULL?ys->active_frame->buffer:NULL;
     if(buffer !=NULL && buffer->has_selection){
-        selection = get_sel_text(buffer);
+        selection = yed_get_selection_text(buffer);
         if(selection != NULL && strcmp(selection,"")!=0){
             base64 = b64_encode(selection,strlen(selection));
             out_buffer = malloc(strlen(base64)+20);
